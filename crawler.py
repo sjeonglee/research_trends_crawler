81d76eec22c3d7bdf5f137847a04f5e699f41cb0
Sojeong Lee <sjlee.seoul@kaist.ac.kr>
2021-04-03 19:38:01 +0900

    :sparkles: Leave crawler only

--- crawler.py
+++ crawler.py
@@ -67,38 +67,3 @@ class info_parser():
     # to check if soup is not empty
     def print_soup(self):
         print(self.soup)
-
-if __name__ == '__main__':
-    # url link: Elsevier > Journals > Social Sciences > Geography, Planning and Development
-    url_catalogue = 'https://www.elsevier.com/catalog?producttype=journals&cat0=27390&cat1=28064&cat2=&q=&search=1&imprintname=&categoryrestriction=&sort=datedesc'
-
-    # get journal lists
-    crawler = page_crawler(url_catalogue)
-    parser = info_parser(crawler.get_soup())
-
-    tag = 'h5'
-    tag_class = 'listing-products-info-text-title'
-    journal_links = parser.crawl_link(tag, tag_class)
-    journal_titles = parser.crawl_title(tag, tag_class)
-    # print(journal_links)
-
-    whole_titles = {}
-    # get infos from journals
-    for i in tqdm(range(len(journal_links))):
-        i = 0
-        url_journal = journal_links[i] + "/recent-articles"
-        journal_crawler = page_crawler(url_journal)
-        journal_parser = info_parser(journal_crawler.get_soup())
-
-        tag = 'div'
-        tag_class = 'pod-listing-header'
-        article_titles = journal_parser.crawl_title(tag, tag_class)
-        whole_titles[journal_titles[i]] = article_titles
-    
-    # keywords were not crawled to comply with robots.txt
-    # article_links = journal_parser.crawl_link(tag, tag_class)
-    # print(article_titles)
-    # print(article_links)
-
-    with open("0402_article_lists.json", 'w') as json_file:
-        json.dump(whole_titles, json_file)
